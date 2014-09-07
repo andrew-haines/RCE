771e17106dce2a495d070e303703f5feb13bc639
Andrew Haines <ajh21_48@hotmail.com>
2014-09-07 12:02:45 +0100

    Adding protostuff to guice confid

--- main/src/test/java/com/haines/ml/rce/main/factory/TestGuiceRCEApplicationFactory.java
+++ main/src/test/java/com/haines/ml/rce/main/factory/TestGuiceRCEApplicationFactory.java
@@ -1,15 +1,13 @@
 package com.haines.ml.rce.main.factory;
 
-import java.nio.ByteBuffer;
 import java.nio.channels.ServerSocketChannel;
 
-import com.dyuproject.protostuff.ProtostuffIOUtil;
 import com.google.inject.Module;
 import com.google.inject.Provider;
+import com.haines.ml.rce.io.protostuff.ProtostuffEventMarshalBuffer;
 import com.haines.ml.rce.main.guice.AsyncPipelineRCEConfigConfiguredInitiationModule;
 import com.haines.ml.rce.model.EventMarshalBuffer;
-import com.haines.ml.rce.model.UnMarshalableException;
-import com.haines.ml.rce.test.TestEvent;
+import com.haines.ml.rce.test.model.TestEvent;
 
 public class TestGuiceRCEApplicationFactory extends GuiceRCEApplicationFactory{
 
@@ -23,28 +21,9 @@ public class TestGuiceRCEApplicationFactory extends GuiceRCEApplicationFactory{
 
 			@Override
 			public EventMarshalBuffer<TestEvent> get() {
-				return new TestEventMarshalBuffer();
+				return new ProtostuffEventMarshalBuffer<>(TestEvent.getSchema());
 			}
 			
 		});
 	}
-	
-	private static class TestEventMarshalBuffer implements EventMarshalBuffer<TestEvent>{
-
-		private com.haines.ml.rce.test.model.TestEvent event = new com.haines.ml.rce.test.model.TestEvent();
-		
-		@Override
-		public boolean marshal(ByteBuffer content) {
-			ProtostuffIOUtil.mergeFrom(content.array(), event, event.cachedSchema());
-			return true;
-		}
-
-		@Override
-		public TestEvent buildEventAndResetBuffer() throws UnMarshalableException {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	}
-
 }
